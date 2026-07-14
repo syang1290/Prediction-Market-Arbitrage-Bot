7d3558c899b97e2713e966c7e186435699417524
syang1290 <stevey13@uci.edu>
2026-07-13 23:13:09 -0700

    Implemented constructor for Arbitrage Event class

--- engine.py
+++ engine.py
@@ -1,1 +1,18 @@
 # aysncio
+
+class Event:
+    def __init__(self, event_id: int, platform: str, buy: dict, sell: dict):
+        self.event_id = event_id
+        self.platform = platform
+
+        # event_id is the ID of the event
+        # platform describes which platform (polymarket/kalshi)
+
+        # buy are prices that people are willing to buy at
+        # key is price, value is amount of shares
+        self.buy = buy
+
+        # sell are prices that people are willing to sell at
+        # key is price, value is amount of shares
+        self.sell = sell
+    
